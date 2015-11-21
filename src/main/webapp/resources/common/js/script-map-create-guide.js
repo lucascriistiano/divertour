bea948ce0e5f1d70ba81fc99026ae7a5b1dc0221
Lucas Cristiano Calixto Dantas <lucascristiano27@gmail.com>
2015-11-21 04:03:10 -0300

    Remoção de partes desnecessárias

--- src/main/webapp/resources/common/js/script-map-create-guide.js
+++ src/main/webapp/resources/common/js/script-map-create-guide.js
@@ -1,16 +1,10 @@
-function initializeMap() {
-	getLocation();
-}
-
-$(document).ready(initializeMap);
-//google.maps.event.addDomListener(window, 'load', initializeMap);
+$(document).ready(getLocation);
 
 function getLocation() {
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(success, error);
 	} else {
-		// Position unavailable on navigator		
-		error(2);
+		error(2); // Position unavailable on navigator
 	}
 }
 
@@ -54,82 +48,15 @@ var map;
 var directionsDisplay;
 var directionsService;
 
-//var searchBox;
-//
-//// Searchbar
 function initAutocomplete(mapOptions) {
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 	directionsService = new google.maps.DirectionsService();
 	directionsDisplay = new google.maps.DirectionsRenderer();
 	directionsDisplay.setMap(map);
 //	directionsDisplay.setPanel(document.getElementById("directionsPanel"));
-
-//	// Create the search box and link it to the UI element.
-//	var input = document.getElementById('pac-input');
-//	map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
-//	searchBox = new google.maps.places.SearchBox(input);
-////	input.style.display = 'block';
-//
-//	// Bias the SearchBox results towards current map's viewport.
-//	map.addListener('bounds_changed', function() {
-//		searchBox.setBounds(map.getBounds());
-//	});
-//
-//	var markers = [];
-//	// [START region_getplaces]
-//	// Listen for the event fired when the user selects a prediction and
-//	// retrieve
-//	// more details for that place.
-//	searchBox.addListener('places_changed', function() {
-//		var places = searchBox.getPlaces();
-//
-//		if (places.length == 0) {
-//			return;
-//		}
-//
-//		// Clear out the old markers.
-//		markers.forEach(function(marker) {
-//			marker.setMap(null);
-//		});
-//		markers = [];
-//
-//		// For each place, get the icon, name and location.
-//		var bounds = new google.maps.LatLngBounds();
-//		places.forEach(function(place) {
-//			var icon = {
-//				url : place.icon,
-//				size : new google.maps.Size(71, 71),
-//				origin : new google.maps.Point(0, 0),
-//				anchor : new google.maps.Point(17, 34),
-//				scaledSize : new google.maps.Size(25, 25)
-//			};
-//
-//			// Create a marker for each place.
-//			markers.push(new google.maps.Marker({
-//				map : map,
-//				icon : icon,
-//				title : place.name,
-//				position : place.geometry.location
-//			}));
-//
-//			if (place.geometry.viewport) {
-//				// Only geocodes have viewport.
-//				bounds.union(place.geometry.viewport);
-//			} else {
-//				bounds.extend(place.geometry.location);
-//			}
-//		});
-//		map.fitBounds(bounds);
-//	});
-//	// [END region_getplaces]
 }
 
-$(function() {
-	$('#pac-input').show();
-})
-
 var markers = {};
-
 
 function addMarker() {
 	var strPlace = $('#selected-place-json').val();
