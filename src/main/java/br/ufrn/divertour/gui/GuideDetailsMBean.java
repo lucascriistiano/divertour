043ebc44f4c4ec585e8c8f354ecc6f6f5649825e
Stefano10 <momoloss10@gmail.com>
2015-12-08 22:44:34 -0300

    detalhamento roteiros

--- src/main/java/br/ufrn/divertour/gui/GuideDetailsMBean.java
+++ src/main/java/br/ufrn/divertour/gui/GuideDetailsMBean.java
@@ -6,25 +6,52 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import br.ufrn.divertour.model.Guide;
+import br.ufrn.divertour.service.GuideService;
+import br.ufrn.divertour.service.PlaceService;
+import br.ufrn.divertour.service.UserService;
 
 @ManagedBean(name = "guideDetailsMBean")
 @ViewScoped
 public class GuideDetailsMBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	private GuideService guideService;
+	
+	private String guideId;
 	
 	@ManagedProperty(value="#{searchMBean.selectedItem}")
 	private Guide selectedGuide;
 	
-	public GuideDetailsMBean() {}
+	public GuideDetailsMBean() {
+		@SuppressWarnings("resource")
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		this.guideService = (GuideService) context.getBean(GuideService.class);			
+	}
+		
 	
+	public String getGuideId() {
+		return guideId;
+	}
+
+	public void setGuideId(String guideId) {
+		this.guideId = guideId;
+	}
+
 	public Guide getSelectedGuide() {
 		return selectedGuide;
 	}
 
 	public void setSelectedGuide(Guide selectedGuide) {
 		this.selectedGuide = selectedGuide;
+	}	
+	
+	public void loadData() {
+		//Load selected guide info
+		this.selectedGuide = guideService.findById(guideId);
 	}
 	
 }
